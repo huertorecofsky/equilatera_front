df80f88fea1f5647f468d625e5a7f74d171c8a51
huertorecofsky <huerrd@gmail.com>
2019-09-30 22:41:49 -0300

    commit initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import ToDoForm from './components/ToDoForm';
+
+function App() {
+  return (
+    <div>
+      <ToDoForm
+      pregunta = "¿Cree que su empresa es igualitaria?"/>
+
+    </div>
+  );
+}
+
+export default App;
